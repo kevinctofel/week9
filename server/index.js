59c2554c0a097869d1b67d4b1a75ec0d8141225a
Frank Stepanski <fsjay@hotmail.com>
2021-04-14 16:43:15 -0400

    updates

--- server/index.js
+++ server/index.js
@@ -8,13 +8,27 @@
 // create a /health route just to check that our API is healthy
 
 /*
+
  HOMEWORK
+
+  /server
 
   1. create a route for editing posts (complete and edit text)
   2. create a route to delete a post
-  3. add appropriate fetch requests to make your todo app to use data from our server
-     whenever we add or edit or delete
-*/
+  
+  /client
+
+  1. Add appropriate fetch requests to make your todo app to use data from our server
+     whenever we need updated data: adding, editing or deleting or initial read
+
+     Example:
+
+        a. initial read - do a fetch
+        b. add a post - get data from user, do a post request, have the server add to server todos object
+           and return and updated todos array
+        c. edit a post (complete or edit text) - get updates from user, do a put request, have the server
+           update the todos object and return back the updated todos array
+ */
 
 // app.listen on port 8080 (client is running on port 3000)
 
